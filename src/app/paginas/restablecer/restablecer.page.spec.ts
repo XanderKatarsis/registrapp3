7b36690b5fe2ab0b449ae1beca7e6d84a4e24a65
XanderKatarsis <a.monares.tobar@gmail.com>
2023-10-16 22:05:50 -0300

    xxxx

--- src/app/paginas/restablecer/restablecer.page.spec.ts
+++ src/app/paginas/restablecer/restablecer.page.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RestablecerPage } from './restablecer.page';
 
+
 describe('RestablecerPage', () => {
   let component: RestablecerPage;
   let fixture: ComponentFixture<RestablecerPage>;
@@ -15,3 +16,7 @@ describe('RestablecerPage', () => {
     expect(component).toBeTruthy();
   });
 });
+function async(arg0: () => void): jasmine.ImplementationCallback {
+  throw new Error('Function not implemented.');
+}
+
